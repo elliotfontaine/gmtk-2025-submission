d49e37f4b90544908dee7ccdeea0af30a3746eed
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-07-31 00:10:47 +0200

    Update world.gd

--- world.gd
+++ world.gd
@@ -16,8 +16,8 @@ func add_creature(nb:int):
 	for creature :Creature in creatures:
 		var angle = (2 * PI * i) / creature_amount
 		i+= 1
-		creature.position.x = max(30*creature_amount,100) * cos(angle)
-		creature.position.y = max(30*creature_amount,100) * sin(angle)
+		creature.position.x = max(30*creature_amount,150) * cos(angle)
+		creature.position.y = max(30*creature_amount,150) * sin(angle)
 
 
 func _on_button_add_pressed() -> void:
