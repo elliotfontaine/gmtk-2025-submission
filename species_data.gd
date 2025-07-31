f5aaf42c88d123a13bdb4c6f7a9384c9464fd972
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-07-31 12:22:22 +0200

    Species data (#7)
    * species data resource and data manager
    
    on game start, species data manager iterates over the species data and adds the resources to arrays
    
    * Update species_data.gd

--- species_data.gd
+++ species_data.gd
@@ -4,8 +4,17 @@ class_name SpeciesData
 @export var id :String
 @export var title :String
 
+enum TYPES {small, medium, large, plant, animal, insect}
+
 @export var type :Array[TYPES]
 
 @export var texture :CompressedTexture2D
 
-enum TYPES {small, medium, large, plant, animal, insect}
+enum RARITIES {common, uncommon, rare}
+@export var rarity :RARITIES
+
+@export var range :int
+@export var value :int
+
+#@export var sfx_trigger :AudioStream
+#@export var sfx_death :AudioStream
