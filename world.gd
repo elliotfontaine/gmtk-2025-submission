f9a04800e9cd18dafd05412b44dba2608dbcccf5
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-07-31 09:54:15 +0200

    Creature types and portraits (#5)
    * quick prototype assets
    
    * text field to specify type, bad code to associate type to image
    
    * scaling--
    
    * project stretch mode to canvas_items - that looks much better for the assets

--- world.gd
+++ world.gd
@@ -2,16 +2,29 @@ extends Node2D
 
 const CREATURE = preload("res://creature.tscn")
 
+const BUNNY = preload("res://assets/bunny.png")
+const GRASS = preload("res://assets/grass.png")
+const WORM = preload("res://assets/worm.png")
+
 const minimum_loop_size :int = 150
+
+@onready var text_edit: TextEdit = %TextEdit
 
 var creatures :Array[Creature]
 
 
-func add_creature(nb:int):
+func add_creature(nb:int,type:String):
 	for i in nb:
 		var new_creature := CREATURE.instantiate()
 		add_child(new_creature)
 		creatures.append(new_creature)
+		new_creature.type = type
+		if type == "bunny":
+			new_creature.get_child(0).texture = BUNNY
+		elif type == "worm":
+			new_creature.get_child(0).texture = WORM
+		else:
+			new_creature.get_child(0).texture = GRASS
 	
 	var creature_amount = creatures.size()
 	var i :int = 0
@@ -23,7 +36,7 @@ func add_creature(nb:int):
 
 
 func _on_button_add_pressed() -> void:
-	add_creature(1)
+	add_creature(1,text_edit.text)
 
 func run_loop() -> void:
 	for creature :Creature in creatures:
