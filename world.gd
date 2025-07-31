b719a1a0fdceb4a794a3c6c875fcc320deafb28f
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-07-31 14:02:05 +0200

    zooms out as the loop gets bigger
    adaptative zoom: to adjust further once we have a better idea of the size of assets

--- world.gd
+++ world.gd
@@ -9,11 +9,12 @@ const WORM = preload("res://assets/worm.png")
 const minimum_loop_size :int = 150
 
 @onready var text_edit: TextEdit = %TextEdit
+@onready var camera_2d: Camera2D = %Camera2D
 
 var creatures :Array[Creature]
 
 
-func add_creature(nb:int,type:String):
+func add_creature(nb:int,type:String) -> void:
 	for i in nb:
 		var new_creature := CREATURE.instantiate()
 		add_child(new_creature)
@@ -26,6 +27,11 @@ func add_creature(nb:int,type:String):
 		else:
 			new_creature.get_child(0).texture = GRASS
 	
+	update_creature_positions()
+
+
+##to call whenever you affect the number of creatures in the loop 
+func update_creature_positions() -> void:
 	var creature_amount = creatures.size()
 	var i :int = 0
 	for creature :Creature in creatures:
@@ -33,10 +39,15 @@ func add_creature(nb:int,type:String):
 		i+= 1
 		creature.position.x = max(30*creature_amount,minimum_loop_size) * cos(angle)
 		creature.position.y = max(30*creature_amount,minimum_loop_size) * sin(angle)
+	
+	##adaptative zoom: to adjust further once we have a better idea of the size of assets
+	var zoom :float = max(1.0 - (0.015 * creature_amount),0.3)
+	camera_2d.zoom = Vector2(zoom,zoom)
 
 
 func _on_button_add_pressed() -> void:
 	add_creature(1,text_edit.text)
+
 
 func run_loop() -> void:
 	for creature :Creature in creatures:
