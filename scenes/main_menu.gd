a9000a705f789411de43c85b5890feea11bcab40
samjmerrick <samjmerrick@gmail.com>
2025-08-01 14:39:26 +0100

    Add accept sound on main menu button

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var sfx_accept: AudioStreamPlayer2D = $SFX_Accept
 
 func _on_button_play_pressed() -> void:
 	SceneChanger.change_to(SceneChanger.MainScenes.WORLD)
+	sfx_accept.play()
