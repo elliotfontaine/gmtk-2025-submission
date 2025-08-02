93eac6b74dcbf00da0addf1b44d946fd3788670e
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-08-02 20:57:07 +0200

    added money

--- scenes/ui/shop_panel.gd
+++ scenes/ui/shop_panel.gd
@@ -1,8 +1,10 @@
 extends PanelContainer
+class_name ShopPanel
 
-signal floating_creature_asked(species: SpeciesResource)
+signal floating_creature_asked(item:ShopItem)
 signal item_hovered(species: SpeciesResource)
 signal item_exited
+signal rerolled
 
 ## Creative for the shop means you can add any registered creature for free
 @export var creative: bool = false
@@ -11,9 +13,9 @@ signal item_exited
 
 @onready var grid_container := %GridContainer
 @onready var sfx_click: AudioStreamPlayer = %SFX_Click
+@onready var re_roll: Button = %ReRoll
 
 const ITEM_SCENE := preload("res://scenes/ui/shop_item.tscn")
-
 
 func _ready() -> void:
 	if creative:
@@ -25,18 +27,22 @@ func _ready() -> void:
 		# TODO: iDK, I guess maybe it should me managed by the World Scene, since
 		# it's gonna fill it mutiple times.
 
-func _on_item_hovered(item) -> void:
+func _on_item_hovered(item:ShopItem) -> void:
 	# TODO: show "- Price" after currency count
 	item_hovered.emit(item.species)
 
-func _on_item_exited(item) -> void:
+func _on_item_exited(item:ShopItem) -> void:
 	item_exited.emit()
-	
-func _on_item_pressed(item) -> void:
-	floating_creature_asked.emit(item.species)
+
+func _on_item_pressed(item:ShopItem) -> void:
+	floating_creature_asked.emit(item) #connected in inspector
 	sfx_click.play() 
 
 func _on_re_roll_pressed() -> void:
+	rerolled.emit() #connected in inspector to world
+
+##called by world
+func do_reroll() -> void:
 	clear_items()
 	populate_shop()
 
@@ -48,7 +54,7 @@ func get_shop_items() -> Array[Node]:
 
 func clear_items() -> void:
 	print_debug("clear")
-	for item in grid_container.get_children():
+	for item:ShopItem in grid_container.get_children():
 		grid_container.remove_child(item)
 		item.queue_free()
 
@@ -56,7 +62,7 @@ func create_new_shop_item(id:int) -> void:
 	var species: SpeciesResource = Constants.get_species_by_id(id)
 	var new_item: Control = ITEM_SCENE.instantiate()
 	new_item.species = species
-	new_item.price = randi_range(10,999)
+	new_item.price = randi_range(40,199)
 	add_shop_item(new_item)
 	new_item.mouse_entered.connect(_on_item_hovered.bind(new_item))
 	new_item.mouse_exited.connect(_on_item_exited.bind(new_item))
