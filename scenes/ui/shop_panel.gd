a7a8cd77cc3ab57e4882ea8862c7c1e9c84d21bd
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2025-08-02 05:53:43 +0200

    feat: improve the grid display in the shop

--- scenes/ui/shop_panel.gd
+++ scenes/ui/shop_panel.gd
@@ -2,8 +2,10 @@ extends PanelContainer
 
 signal floating_creature_asked(species: SpeciesResource)
 
-# debug for the shop means creative mode: you can add stuff for free
-@export var debug: bool = true
+## Creative for the shop means you can add any registered creature for free
+@export var creative: bool = true
+## Should the creature count stop at the 6 first instead of all registered
+@export var stop_at_six: bool = true
 
 @onready var grid_container := %GridContainer
 @onready var sfx_click: AudioStreamPlayer = %SFX_Click
@@ -11,9 +13,9 @@ signal floating_creature_asked(species: SpeciesResource)
 const ITEM_SCENE := preload("res://scenes/ui/shop_item.tscn")
 
 func _ready() -> void:
-	clear()
-	if debug:
-		populate_debug_shop()
+	clear_items()
+	if creative:
+		populate_creative_shop()
 
 func _on_item_hovered(item) -> void:
 	# TODO: show "- Price" after currency count
@@ -31,16 +33,20 @@ func _on_item_pressed(item) -> void:
 func add_shop_item(shop_item: Control) -> void:
 	grid_container.add_child(shop_item)
 
-func clear() -> void:
+func get_shop_items() -> Array[Node]:
+	return grid_container.get_children()
+
+func clear_items() -> void:
 	print_debug("clear")
 	for item in grid_container.get_children():
 		grid_container.remove_child(item)
 		item.queue_free()
 	
-func populate_debug_shop() -> void:
+func populate_creative_shop() -> void:
 	print_debug("populate in debug mode")
-	#var species: Array[SpeciesResource] = CreatureDataManager.all_species
 	for id in Constants.SPECIES.values():
+		if stop_at_six and get_shop_items().size() >= 6:
+			break
 		if id == Constants.SPECIES.NONE:
 			continue
 		var species: SpeciesResource = Constants.get_species_by_id(id)
