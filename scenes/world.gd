959aad78830ea21d92b92c20f1f8e6f61c3e23cc
VTinarrage <45184288+VTinarrage@users.noreply.github.com>
2025-08-03 15:13:28 +0200

    Background and initial tuto (#65)
    * Update bg.png
    
    * tutorial text

--- scenes/world.gd
+++ scenes/world.gd
@@ -32,6 +32,7 @@ const final_level: int = 20
 @onready var victory_ui: Control = %VictoryScreen
 @onready var victory_loop_label: Label = %VictoryLoopLabel
 @onready var label_loop: Label = %LabelLoop
+@onready var h_box_tutorial: HBoxContainer = %HBoxTutorial
 
 ##placeholder system: length of wait times 
 const game_speeds: Array[float] = [0.8, 0.6, 0.4, 0.2]
@@ -68,6 +69,7 @@ func _ready() -> void:
 
 ##to call whenever you affect the number of creatures in the loop 
 func update_creature_positions(show_empty_slots: bool = false) -> void:
+	h_box_tutorial.hide()
 	for slot in empty_slots:
 		slot.queue_free()
 	empty_slots.clear()
