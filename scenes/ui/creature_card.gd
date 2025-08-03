ea89bdba3bb1770638be20484e2c1d1fe70f0f45
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2025-08-03 19:47:29 +0200

    set rarity backgrounds

--- scenes/ui/creature_card.gd
+++ scenes/ui/creature_card.gd
@@ -1,6 +1,11 @@
 @tool
 class_name CreatureCard extends PanelContainer
 
+const no_bg = preload("res://assets/sprites/ui/creature_background.png")
+const common_bg = preload("res://assets/sprites/ui/rarity_common.png")
+const uncommon_bg = preload("res://assets/sprites/ui/rarity_uncommon.png")
+const rare_bg = preload("res://assets/sprites/ui/rarity_rare.png")
+
 @export var species: SpeciesResource:
 	set(value):
 		species = value
@@ -17,8 +22,20 @@ func _set_species(sp: SpeciesResource) -> void:
 		%CreatureTitle.hide()
 		%MiddleLine.hide()
 		%RangeIcon.hide()
+		%CreatureBackground.texture = no_bg
+		
 	else:
 		%CreatureSprite.texture = sp.texture
+		
+		match sp.rarity:
+			Constants.RARITIES.COMMON:
+				%CreatureBackground.texture = common_bg
+			Constants.RARITIES.UNCOMMON:
+				%CreatureBackground.texture = uncommon_bg
+			Constants.RARITIES.RARE:
+				%CreatureBackground.texture = rare_bg
+			_:
+				%CreatureBackground.texture = no_bg
 		%CreatureTitle.text = sp.title
 		%Size.text = Constants.size_strings[sp.size]
 		%Family.text = Constants.family_strings[sp.family]
--- scenes/ui/shop_item.gd
+++ scenes/ui/shop_item.gd
@@ -2,6 +2,11 @@
 extends Control
 class_name ShopItem
 
+const no_bg = preload("res://assets/sprites/ui/creature_background.png")
+const common_bg = preload("res://assets/sprites/ui/rarity_common.png")
+const uncommon_bg = preload("res://assets/sprites/ui/rarity_uncommon.png")
+const rare_bg = preload("res://assets/sprites/ui/rarity_rare.png")
+
 @onready var h_box_container: HBoxContainer = %HBoxContainer
 @onready var texture_button: TextureButton = %TextureButton
 
@@ -11,6 +16,18 @@ signal pressed
 	set(value):
 		species = value
 		%CreatureSprite.texture = value.texture if value != null else null
+		if value == null:
+			%CreatureBackground.texture = no_bg
+		else:
+			match value.rarity:
+				Constants.RARITIES.COMMON:
+					%CreatureBackground.texture = common_bg
+				Constants.RARITIES.UNCOMMON:
+					%CreatureBackground.texture = uncommon_bg
+				Constants.RARITIES.RARE:
+					%CreatureBackground.texture = rare_bg
+				_:
+					%CreatureBackground.texture = no_bg
 
 @export var price: int = 101:
 	get: return int(%Price.text)
