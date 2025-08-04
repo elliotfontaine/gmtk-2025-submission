9f977dc3578c48948d4e1bf01586edb08c391a68
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2025-08-04 22:29:43 +0200

    feat: add version tag to main menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,14 @@
 extends Control
 
 @onready var settings_menu: Control = %SettingsMenu
+@onready var version_tag: Label = %VersionTag
+
+func _ready() -> void:
+	var project_version: String = ProjectSettings.get_setting("application/config/version")
+	if project_version != "":
+		version_tag.text = "vers. " + project_version
+	else:
+		version_tag.text = "Unknown version"
 
 func _on_button_play_pressed() -> void:
 	SceneChanger.change_to(SceneChanger.MainScenes.WORLD)
